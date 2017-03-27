d727dee9edb035bd1d6de4db7140e775fa8e287e
Diky Arga <dikyarga.id@gmail.com>
2017-03-27 14:59:44 +0800

    X and Y axis release

--- script.js
+++ script.js
@@ -11,18 +11,18 @@ let svg = d3.select('#results')
     .append('svg')
     .attr('width', width)
     .attr('height', height)
+    .style('padding', '100px 50px')
     .style('background', '#FAFAFA')
-
-
+    .style('border', '1px solid black')
 
 // Data reloading
 let reload = () => {
     // Your data parsing here...
     d3.tsv('afcw-results.tsv', (rows) => {
-        console.log(rows);
+        // console.log(rows);
         let dataset = []
         rows.map((row) => {
-          dataset.push(row.GoalsScored)
+            dataset.push(row.GoalsScored)
         })
         redraw(dataset)
     })
@@ -33,7 +33,24 @@ let redraw = (dataset) => {
     // Your data to graph here
     const yScale = d3.scaleLinear()
         .domain([0, d3.max(dataset)])
-        .range([0, 300])
+        .range([0, height])
+
+    const xScale = d3.scaleLinear()
+        .domain([dataset.length, 0])
+        .range([width, 0])
+    //
+
+    var yAxis = d3.axisLeft(yScale);
+    var xAxis = d3.axisBottom(xScale);
+
+    svg.append('g')
+       .call(yAxis)
+       .attr('transform', 'translate(20, 0)')
+
+    svg.append('g')
+       .call(xAxis)
+      //  .attr('transform', 'translate(0, 280)')
+
 
     svg.selectAll('rect')
         .data(dataset)
@@ -43,8 +60,7 @@ let redraw = (dataset) => {
             return i * 20
         })
         .attr('y', (d) => {
-            console.log('isi day', d);
-            return 300 - yScale(d)
+            return height - yScale(d)
         })
         .attr('width', 20)
         .attr('height', (d) => {
